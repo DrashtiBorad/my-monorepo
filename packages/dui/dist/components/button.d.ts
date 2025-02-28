8808e678eb3fc542e42c5d96733a0cc6393e6178
DrashtiBorad <djborad0890@gmail.com>
2025-02-28 13:18:45 +0530

    chore: setup automatic versioning and publishing

---
+++ packages/dui/dist/components/button.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+interface ButtonProps {
+    label: string;
+}
+export declare const Button: React.FC<ButtonProps>;
+export {};
+//# sourceMappingURL=button.d.ts.map
---
+++ packages/dui/dist/components/button.js
@@ -0,0 +1,4 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+export const Button = ({ label }) => {
+    return _jsx("button", { className: "btn", children: label });
+};
